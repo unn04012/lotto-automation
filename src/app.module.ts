cbce893e0f260c298ae93253a50863ce39f5a6f1
unn04012 <unn04012@naver.com>
2025-05-27 20:47:00 +0900

    feat: added serverless package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LotteryAgentModule } from './lottery-agent/lottery-agent.module';
 import { ConfigModule } from '@nestjs/config';
 import { LottoModule } from './lotto/lotto.module';
@@ -8,7 +6,5 @@ import { NotificationModule } from './notification/notificaiton.module';
 
 @Module({
   imports: [ConfigModule, LotteryAgentModule, LottoModule, NotificationModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let server: Handler;
 
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
